2a85d3cb84f83e74a570dd5fd2fcd6bcf5f73f33
dwanna01 <30464706+dwanna01@users.noreply.github.com>
2017-08-16 18:34:05 +1000

    Test Comit
    Test

--- ITC205 201760 Codebase/src/bcccp/carpark/CarSensor.java
+++ ITC205 201760 Codebase/src/bcccp/carpark/CarSensor.java
@@ -75,6 +75,9 @@ public class CarSensor extends JFrame implements ICarSensor {
 		detectorButton.setBackground(Color.RED);
 		detectorButton.setText("No Car Detected");
 		contentPane.add(detectorButton);
+		
+		if(1==1)
+			int x=0;
 	}
 	
 	public void registerResponder(ICarSensorResponder responder) {
