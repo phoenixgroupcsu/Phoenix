e2774e332a6bff9bb1aedc37d398275d23d6cfa0
kdkrajesh <30462661+kdkrajesh@users.noreply.github.com>
2017-08-27 20:03:00 +1000

    comment added on constructor body
    comment added on constructor body

--- ITC205 201760 Codebase/src/bcccp/tickets/adhoc/AdhocTicketFactory.java
+++ ITC205 201760 Codebase/src/bcccp/tickets/adhoc/AdhocTicketFactory.java
@@ -14,6 +14,7 @@ public class AdhocTicketFactory implements IAdhocTicketFactory {
 	@Override
 	public IAdhocTicket make(String carparkId, int ticketNo) {
 		// TODO Auto-generated method stub
+		// initializing variables with data passed in parameters
 		this.carparkId = carparkId;
 		this.ticketNo = ticketNo;
 		return null;
