c58ec909a1ee140ec9e393f561da6d0f39833dee
kdkrajesh <30462661+kdkrajesh@users.noreply.github.com>
2017-09-03 23:07:39 +1000

    comment added before class definition
    comment edited before class definition

--- ITC205 201760 Codebase/src/bcccp/tickets/season/SeasonTicket.java
+++ ITC205 201760 Codebase/src/bcccp/tickets/season/SeasonTicket.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/** class SeasonTicket begins from here */
+/** defining class SeasonTicket which implements ISeasonTicket */
 public class SeasonTicket implements ISeasonTicket {
 	
 	private List<IUsageRecord> usages;
